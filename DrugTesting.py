2734dcdb4c4021536997e8de8e33f47b63e68719
james <jameschapman17@gmail.com>
2023-05-12 15:04:34 -0700

    Initial Commit

---
+++ DrugTesting.py
@@ -0,0 +1,97 @@
+# Certainly! Here is some example code that demonstrates how to use OpenMM to simulate the binding of a small molecule to a collection of proteins:
+
+# ```python
+import simtk.openmm as mm
+from simtk.openmm import app
+from simtk.unit import *
+import numpy as np
+
+# Load the protein and ligand structures
+pdb = app.PDBFile('protein.pdb')
+ligand = app.PDBFile('ligand.pdb')
+
+# Create a system object for the protein-ligand complex
+forcefield = app.ForceField('amber99sbildn.xml', 'tip3p.xml')
+system = forcefield.createSystem(pdb.topology, nonbondedMethod=app.PME, constraints=app.HBonds)
+ligand_system = forcefield.createSystem(ligand.topology, nonbondedMethod=app.PME, constraints=app.HBonds)
+
+# Create a CustomIntegrator object to implement the Langevin integrator
+integrator = mm.LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
+
+# Create a simulation object and set the initial positions and velocities
+simulation = app.Simulation(pdb.topology, system, integrator)
+simulation.context.setPositions(pdb.positions)
+simulation.context.setVelocitiesToTemperature(300*kelvin)
+
+# Add the ligand to the simulation context and set its initial position
+ligand_topology = ligand.topology
+ligand_positions = ligand.positions
+ligand_forceindex = simulation.context.getSystem().addForce(mm.CustomExternalForce('0'))
+for atom in ligand_topology.atoms():
+    ligand_forceindex.addParticle(atom.index, [])
+simulation.context.setPositions(ligand_positions)
+
+# Create a list of binding site residues based on a cutoff distance from the ligand
+binding_site_cutoff = 5.0*angstroms
+binding_site_residues = []
+for protein_residue in pdb.topology.residues():
+    for ligand_atom in ligand_topology.atoms():
+        for protein_atom in protein_residue.atoms():
+            if (ligand_atom.pos - protein_atom.pos).norm() < binding_site_cutoff:
+                binding_site_residues.append(protein_residue.index)
+                break
+
+# Create a CustomCVForce object to implement the collective variable for binding
+cvforce = mm.CustomCVForce('distance(ligand_index, residue_index) <= binding_site_cutoff')
+cvforce.addGlobalParameter('binding_site_cutoff', binding_site_cutoff)
+cvforce.addPerParticleParameter('ligand_index')
+cvforce.addPerParticleParameter('residue_index')
+cvforce.addCollectiveVariable('distance', 'distance(ligand_index, residue_index)')
+for i in range(ligand_topology.getNumAtoms()):
+    cvforce.addParticle(i, [i, -1])
+for residue_index in binding_site_residues:
+    for atom_index in pdb.topology.residue(residue_index).atoms():
+        cvforce.addParticle(ligand_topology.getNumAtoms() + atom_index.index, [ligand_topology.getNumAtoms() + atom_index.index, residue_index])
+system.addForce(cvforce)
+
+# Set up reporters to output simulation data
+pdb_reporter = app.PDBReporter('output.pdb', 1000)
+state_data_reporter = app.StateDataReporter('state_data.csv', 1000, step=True, time=True, potentialEnergy=True, temperature=True, totalEnergy=True)
+
+# Run the simulation
+simulation.reporters.append(pdb_reporter)
+simulation.reporters.append(state_data_reporter)
+simulation.step(10000)
+
+# Analyze the simulation data
+state_data = np.genfromtxt('state_data.csv', delimiter=',', names=True)
+# binding_energy = np.average(state_data['potentialEnergy
+
+# Compute the binding free energy using the weighted histogram analysis method (WHAM)
+from openmmtools import states, mcmc, multistate
+from pymbar import timeseries
+
+# Set up the multistate simulation object
+n_states = len(binding_site_residues) + 1
+thermodynamic_states = [states.ThermodynamicState(system=system, temperature=300*kelvin) for _ in range(n_states)]
+sampler_states = [states.SamplerState(positions=pdb.positions, box_vectors=pdb.topology.getPeriodicBoxVectors()) for _ in range(n_states)]
+for i in range(1, n_states):
+    sampler_states[i].positions = ligand_positions
+mcmc_moves = mcmc.LangevinSplittingDynamicsMove(timestep=2.0*femtoseconds, n_steps=500)
+simulation = multistate.MultiStateSimulation(thermodynamic_states, sampler_states, mcmc_moves)
+
+# Set up the WHAM object
+kT = 300.0 * kelvin * Avogadro / calorie
+min_energy = min(state_data['potentialEnergy'])
+max_energy = max(state_data['potentialEnergy'])
+nbins = 100
+wham = multistate.WHAM(state_data['potentialEnergy'], simulation.thermodynamic_states, kT, nbins=nbins, min_energy=min_energy, max_energy=max_energy)
+
+# Compute the binding free energy
+binding_free_energy, error = wham.get_free_energy()
+print('Binding free energy: %f +/- %f kcal/mol' % (binding_free_energy / kilocalories_per_mole, error / kilocalories_per_mole))
+```
+
+This code sets up a simulation of a protein-ligand complex and uses a collective variable force to detect the binding of the ligand to the protein. The simulation is run for a fixed number of steps, and the results are then analyzed using the weighted histogram analysis method (WHAM) to compute the binding free energy.
+
+Note that this is just an example and would need to be adapted to your specific use case, but it should provide a starting point for working with OpenMM.
